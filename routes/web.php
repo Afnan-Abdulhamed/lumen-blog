e0fbe7e2fc7c890425d43d0cd6842bd9bd2c4a47
Afnan.Abdelhameed <nu7hla3vuudg3oaf27uuexvvouffwbt6smwnene7swjzynzv3wfa>
2022-06-19 03:01:17 +0200

    add first API

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('/posts', 'PostController@listPosts');
+
+$router->post('/posts/get', function () {
+    return 'Hello World';
+});
